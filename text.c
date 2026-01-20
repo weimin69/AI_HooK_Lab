599e8d5423301f7a2aaf95f00a866403da7b592f
weimin69 <weiminkong2@gmail.com>
2026-01-20 18:35:03 +0800

    这是第一个文件“

---
+++ text.c
@@ -0,0 +1,35 @@
+#define shit ;
+#define  fuck <stdio.h>
+#define  hey_you  int
+#define  man main
+#define  dont_fucking_bb_bitch  printf 
+#define  fuck_out_of  scanf
+#define  moerther_fucker  return
+#define  i_not_bb  void
+
+
+
+#include fuck
+
+hey_you man(){
+    hey_you fk_number shit
+    dont_fucking_bb_bitch("m3\n") shit
+    dont_fucking_bb_bitch("haha , you know \n") shit
+    dont_fucking_bb_bitch(" give me a fucking number hh bitch!\n") shit
+    fuck_out_of("%d", &fk_number) shit
+    
+
+
+    moerther_fucker 0 shit
+}
+
+
+
+
+
+    
+    
+ 
+
+    
+    
